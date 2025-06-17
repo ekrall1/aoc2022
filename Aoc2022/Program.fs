b943669a128a6995fb2d3f1073632fe98a0159ca
Eric Krall <krall.eric@gmail.com>
2025-06-17 15:01:44 -0400

    Aoc2022 initial commit

---
+++ Aoc2022/Program.fs
@@ -0,0 +1,32 @@
+﻿open Argu
+
+type CLIArguments =
+    | [<Mandatory; AltCommandLine("-d")>] Day of int
+    | [<Mandatory; AltCommandLine("-p")>] Part of int
+    | [<Mandatory; AltCommandLine("-i")>] Input of string
+
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Day _ -> "Specify the day number."
+            | Part _ -> "Specify the part number (1 or 2)."
+            | Input _ -> "Specify the input file path."
+
+[<EntryPoint>]
+let main argv =
+    let parser = ArgumentParser.Create<CLIArguments>(programName = "aoc2022")
+    let results = parser.Parse(argv)
+
+    let day = results.GetResult <@ Day @>
+    let part = results.GetResult <@ Part @>
+    let input = results.GetResult <@ Input @>
+
+    let part1_fn =
+        match day with
+        | 1 -> "not implemented"
+        | _ -> failwith "Invalid day"
+
+    let result = part1_fn
+
+    printfn "Result: %s" result
+    0
---
+++ flake.nix
@@ -0,0 +1,78 @@
+{
+  description = "Flake to build AOC2022.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+          };
+        };
+
+        dotnet = with pkgs; [
+          dotnet-sdk
+        ];
+
+        vscode = (
+          pkgs.vscode-with-extensions.override {
+            vscodeExtensions = with pkgs.vscode-extensions; [
+              bbenoist.nix
+              jnoortheen.nix-ide
+              ionide.ionide-fsharp
+              ms-dotnettools.vscode-dotnet-runtime
+              golang.go
+            ];
+          }
+        );
+
+        azure-cli = pkgs.azure-cli.withExtensions [
+          pkgs.azure-cli-extensions.azure-devops
+        ];
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            dotnet
+            vscode
+            azure-cli
+          ];
+          shellHook = ''
+            echo "Welcome to the F# shell."
+
+            export DOTNET_ROOT=${pkgs.dotnet-sdk}
+            export PATH="$HOME/.dotnet/tools:$PATH"
+
+            if [ ! -f .config/dotnet-tools.json ]; then
+              echo "Initializing dotnet tool manifest..."
+              mkdir -p .config
+              dotnet new tool-manifest
+            fi
+
+            if ! grep -q fantomas .config/dotnet-tools.json; then
+              echo "Installing fantomas..."
+              dotnet tool install fantomas
+            fi
+
+            dotnet add ./Aoc2022/Aoc2022.fsproj package Argu || true
+
+            dotnet tool restore || echo "Could not restore dotnet tools"
+          '';
+        };
+      }
+    );
+}
