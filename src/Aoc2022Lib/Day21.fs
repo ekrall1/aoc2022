2b4b24690a4e3e4e9887af43bb089ca6c51d15bd
Eric Krall <krall.eric@gmail.com>
2025-09-01 08:35:01 -0400

    day 21 parse input

--- src/Aoc2022Lib/Day21.fs
+++ src/Aoc2022Lib/Day21.fs
@@ -4,9 +4,34 @@
 module Aoc2022Lib.Day21
 
 open System
+open System.Collections.Generic
+open System.Text.RegularExpressions
+
+type ParseError = InvalidLine of string
+
+let parseInput (lines: string list) =
+    let monkeyDict = Dictionary<string, string>()
+    let pattern = @"(^.{4})\:\s(.*$)"
+
+    let rec parseLines (lines: string list) =
+        match lines with
+        | [] -> Ok monkeyDict
+        | hd :: tl ->
+            let m = Regex.Match(hd, pattern)
+
+            if m.Success then
+                monkeyDict.Add(m.Groups.[1].Value, m.Groups.[2].Value)
+                parseLines tl
+            else
+                Error(InvalidLine hd)
+
+    parseLines lines
 
 let part1 (lines: string list) =
-    "Not implemented"
+    let input = parseInput lines
 
-let part2 (lines: string list) =
-    "Not implemented"
+    match input with
+    | Ok -> "Not implemented"
+    | Error e -> $"Error parsing input {e}"
+
+let part2 (lines: string list) = "Not implemented"
